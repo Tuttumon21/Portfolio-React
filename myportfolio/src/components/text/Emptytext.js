35e6c14ce2dc9f00d9e0d97c8a55bc4ab625180f
RakeshKR <Rakeshkr212000@gmail.com>
2023-03-05 00:08:20 +0530

    text added
    empty text shows on empty details portfolio

---
+++ myportfolio/src/components/text/Emptytext.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Emptytext = () => {
+  return (
+    <section>
+      <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
+        <div class="flex flex-col w-full mb-12 text-center">
+          <div class="max-w-5xl text-2xl font-extrabold leading-none tracking-tighter text-neutral-1000 md:text-5xl lg:text-6xl lg:max-w-7xl">
+            <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 selection:bg-white">
+              CHECK BACK SOON!
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Emptytext;
