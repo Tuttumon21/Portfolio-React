aeade644726ddba16972499ddec1217cceb1f3cf
RakeshKR <Rakeshkr212000@gmail.com>
2023-03-09 01:37:44 +0530

    Viewprofile fix
    changed the fetch method type and also checking the user is available or not in database server. if the user available they shows the profile with that user submited styles to showcase the profile.also props calling.

--- myportfolio/src/components/Viewprofile.js
+++ myportfolio/src/components/Viewprofile.js
@@ -1,7 +1,6 @@
-
 import React, { useState, useEffect } from "react";
-import Myportfolio from './Myportfolio'
-import { useParams } from "react-router-dom"
+import Myportfolio from "./Myportfolio";
+import { useParams } from "react-router-dom";
 import Aboutmestyle1 from "./myportfolio/Aboutmestyle1";
 import Aboutmestyle2 from "./myportfolio/Aboutmestyle2";
 import Contactmestyle1 from "./myportfolio/Contactmestyle1";
@@ -14,52 +13,63 @@ import Introstyle2 from "./myportfolio/Introstyle2";
 import Servicestyle1 from "./myportfolio/Servicestyle1";
 import Servicestyle2 from "./myportfolio/Servicestyle2";
 import Servicestyle3 from "./myportfolio/Servicestyle3";
+import Emptytext from "./text/Emptytext";
+
 
 const Viewprofile = () => {
-    const { email } = useParams();
-//    console.log(email)
-    const [details, setDetails] = useState("");
-    useEffect(() => {
-      getDetails();
-    }, []);
-  
-    const getDetails = async () => {
-    //   const user = localStorage.getItem("email");
-      fetch("http://localhost:5000/portfolio/details/" + email )
-        .then((response) => response.json())
-        .then((data) => setDetails(data));
-    };
+  // 
+  const { email } = useParams();
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    async function fetchUser() {
+      const response = await fetch(
+        `http://localhost:5000/portfolio/details/${email}`
+      );
+      const data = await response.json();
+      setUser(data);
+    }
+    fetchUser();
+  }, [email]);
+
+  if (!user) {
+    return <div>Loading... please wait... we are searching requested profile<Emptytext/></div>;
+  }
   return (
-    
-    <div> 
-        
-   
-{details.style === null && <></>}
-          {details.style === "1" && <Intro/>}
-          {details.style === "2" && <Introstyle2 />}
+    <div>
+     
+      {user.style === null && <></>}
+          {user.style === "1" && <Intro phoneNumber={user.phoneNumber} category={user.category} jobtitle={user.jobtitle} firstname={user.firstname} lastname={user.lastname} desc={user.desc} imagePath={user.imagePath}/>}
+          {user.style === "2" && <Introstyle2 imagePath={user.imagePath} firstname={user.firstname} lastname={user.lastname} jobtitle={user.jobtitle} desc={user.desc} linkedin={user.linkedin} facebook={user.facebook} instagram={user.instagram} twitter={user.twitter} />}
+          
 
-          {details.aboutStyle === null && <></>}
-          {details.aboutStyle === "Style#1" && <Aboutmestyle1 />}
-          {details.aboutStyle === "Style#2" && <Aboutmestyle2 />}
+          {user.aboutStyle === null && <></>}
+          {user.aboutStyle === "Style#1" && <Aboutmestyle1 aboutDesc={user.aboutDesc} aboutImage={user.aboutImage} />}
+          {user.aboutStyle === "Style#2" && <Aboutmestyle2 aboutImage={user.aboutImage} aboutDesc={user.aboutDesc}  />}
 
-          {details.serviceStyle === null && <></>}
-          {details.serviceStyle === "Style#1" && <Servicestyle1 />}
-          {details.serviceStyle === "Style#2" && <Servicestyle2 />}
-          {details.serviceStyle === "Style#3" && <Servicestyle3 />}
+          {user.serviceStyle === null && <></>}
+          {user.serviceStyle === "Style#1" && <Servicestyle1 service1name={user.service1name} service1desc={user.service1desc} />}
+          {user.serviceStyle === "Style#2" && <Servicestyle2 service2name={user.service2name} service2desc={user.service2desc} service1name={user.service1name} service1desc={user.service1desc} />}
+          {user.serviceStyle === "Style#3" && <Servicestyle3 service3name={user.service3name} service3desc={user.service3desc} service2name={user.service2name} service2desc={user.service2desc} service1name={user.service1name} service1desc={user.service1desc} />}
 
-          {details.experienceStyle === "Style#1" && <Experiencestyle1 />}
-          {details.experienceStyle === "Style#2" && <Experiencestyle2 />}
-          {details.experienceStyle === "Style#3" ? <Experiencestyle3 /> : <></>}
+          {user.experienceStyle === null && <></>}
+          {user.experienceStyle === "Style#1" && <Experiencestyle1 expe1name={user.expe1name} expe1desc={user.expe1desc} />}
+          {user.experienceStyle === "Style#2" && <Experiencestyle2 expe2name={user.expe2name} expe2desc={user.expe2desc} expe1name={user.expe1name} expe1desc={user.expe1desc} />}
+          {user.experienceStyle === "Style#3" && <Experiencestyle3 expe3name={user.expe3name} expe3desc={user.expe3desc} expe2name={user.expe2name} expe2desc={user.expe2desc} expe1name={user.expe1name} expe1desc={user.expe1desc} />}
 
-          {details.contactsStyle === null && <></>}
-          {details.contactsStyle === "Style#1" && <Contactmestyle1 />}
-          {details.contactsStyle === "Style#2" && <Contactmestyle2 />}
-      
+          {user.contactsStyle === null && <></>}
+          {user.contactsStyle === "Style#1" && <Contactmestyle1 phoneNumber={user.phoneNumber} linkedin={user.linkedin} facebook={user.facebook} instagram={user.instagram} twitter={user.twitter} snapchat={user.snapchat} youtube={user.youtube} />}
+          {user.contactsStyle === "Style#2" && <Contactmestyle2 phoneNumber={user.phoneNumber} linkedin={user.linkedin} facebook={user.facebook} instagram={user.instagram} twitter={user.twitter} snapchat={user.snapchat} youtube={user.youtube} />}
 
-        }
+
+
+
+
+
+
+
     </div>
-  )
-}
+  );
+};
 
-export default Viewprofile
+export default Viewprofile;
