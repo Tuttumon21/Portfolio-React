2f23a22c97da4949560bec5df9f9df1fd3180176
RakeshKR <Rakeshkr212000@gmail.com>
2023-03-09 01:33:55 +0530

    Myportfolio fixed
    changed the calling method ,and implements the props call from varies styles

--- myportfolio/src/components/Myportfolio.js
+++ myportfolio/src/components/Myportfolio.js
@@ -15,6 +15,7 @@ import Servicestyle3 from "./myportfolio/Servicestyle3";
 import Navbar from "./Navbar";
 import Emptytext from "./text/Emptytext";
 
+
 const Myportfolio = () => {
   const [details, setDetails] = useState("");
   useEffect(() => {
@@ -23,6 +24,7 @@ const Myportfolio = () => {
 
   const getDetails = async () => {
     const user = localStorage.getItem("email");
+    console.log(user)
     fetch("http://localhost:5000/portfolio/details/" + user)
       .then((response) => response.json())
       .then((data) => setDetails(data));
@@ -39,27 +41,31 @@ const Myportfolio = () => {
       ) : (
         <div>
           <Navbar />
+          <h3>Name: {details.firstname}</h3>
+          <p>Email: {details.email}</p>
 
           {details.style === null && <></>}
-          {details.style === "1" && <Intro />}
-          {details.style === "2" && <Introstyle2 />}
+          {details.style === "1" && <Intro phoneNumber={details.phoneNumber} category={details.category} jobtitle={details.jobtitle} firstname={details.firstname} lastname={details.lastname} desc={details.desc} imagePath={details.imagePath}/>}
+          {details.style === "2" && <Introstyle2 imagePath={details.imagePath} firstname={details.firstname} lastname={details.lastname} jobtitle={details.jobtitle} desc={details.desc} linkedin={details.linkedin} facebook={details.facebook} instagram={details.instagram} twitter={details.twitter} />}
+          
 
           {details.aboutStyle === null && <></>}
-          {details.aboutStyle === "Style#1" && <Aboutmestyle1 />}
-          {details.aboutStyle === "Style#2" && <Aboutmestyle2 />}
+          {details.aboutStyle === "Style#1" && <Aboutmestyle1 aboutDesc={details.aboutDesc} aboutImage={details.aboutImage} />}
+          {details.aboutStyle === "Style#2" && <Aboutmestyle2 aboutImage={details.aboutImage} aboutDesc={details.aboutDesc}  />}
 
           {details.serviceStyle === null && <></>}
-          {details.serviceStyle === "Style#1" && <Servicestyle1 />}
-          {details.serviceStyle === "Style#2" && <Servicestyle2 />}
-          {details.serviceStyle === "Style#3" && <Servicestyle3 />}
+          {details.serviceStyle === "Style#1" && <Servicestyle1 service1name={details.service1name} service1desc={details.service1desc} />}
+          {details.serviceStyle === "Style#2" && <Servicestyle2 service2name={details.service2name} service2desc={details.service2desc} service1name={details.service1name} service1desc={details.service1desc} />}
+          {details.serviceStyle === "Style#3" && <Servicestyle3 service3name={details.service3name} service3desc={details.service3desc} service2name={details.service2name} service2desc={details.service2desc} service1name={details.service1name} service1desc={details.service1desc} />}
 
-          {details.experienceStyle === "Style#1" && <Experiencestyle1 />}
-          {details.experienceStyle === "Style#2" && <Experiencestyle2 />}
-          {details.experienceStyle === "Style#3" ? <Experiencestyle3 /> : <></>}
+          {details.experienceStyle === null && <></>}
+          {details.experienceStyle === "Style#1" && <Experiencestyle1 expe1name={details.expe1name} expe1desc={details.expe1desc} />}
+          {details.experienceStyle === "Style#2" && <Experiencestyle2 expe2name={details.expe2name} expe2desc={details.expe2desc} expe1name={details.expe1name} expe1desc={details.expe1desc} />}
+          {details.experienceStyle === "Style#3" && <Experiencestyle3 expe3name={details.expe3name} expe3desc={details.expe3desc} expe2name={details.expe2name} expe2desc={details.expe2desc} expe1name={details.expe1name} expe1desc={details.expe1desc} />}
 
           {details.contactsStyle === null && <></>}
-          {details.contactsStyle === "Style#1" && <Contactmestyle1 />}
-          {details.contactsStyle === "Style#2" && <Contactmestyle2 />}
+          {details.contactsStyle === "Style#1" && <Contactmestyle1 phoneNumber={details.phoneNumber} linkedin={details.linkedin} facebook={details.facebook} instagram={details.instagram} twitter={details.twitter} snapchat={details.snapchat} youtube={details.youtube} />}
+          {details.contactsStyle === "Style#2" && <Contactmestyle2 phoneNumber={details.phoneNumber} linkedin={details.linkedin} facebook={details.facebook} instagram={details.instagram} twitter={details.twitter} snapchat={details.snapchat} youtube={details.youtube} />}
 
         </div>
       )}
