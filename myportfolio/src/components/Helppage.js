cb26863807321822a3c18e5bc609f7578293e1d8
RakeshKR <Rakeshkr212000@gmail.com>
2023-03-10 16:55:25 +0530

    useEffects
    portter shows if the details available, other wise shows the Userter. Discord availabe

--- myportfolio/src/components/Helppage.js
+++ myportfolio/src/components/Helppage.js
@@ -1,28 +1,54 @@
 // import React from 'react'
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Dischelp from "./Helppage/Dischelp";
 import Portter from "./Helppage/Portter";
 import Userter from "./Helppage/Userter";
 import Navbar from "./Navbar";
 
 const Helppage = () => {
+    const [details, setDetails] = useState("");
+    const [users, setUsers] = useState("");
+
+    useEffect(() => {
+        getPort();
+        getUsers();
+      }, []);
+    
+      const getPort = async () => {
+        const user = localStorage.getItem("email");
+        console.log(user)
+        fetch("http://localhost:5000/portfolio/details/" + user)
+          .then((response) => response.json())
+          .then((data) => setDetails(data));
+        //   console.log(details)
+      };
+    //   useEffect(() => {
+        
+    //   }, []);
+    
+      const getUsers = async () => {
+        const user = localStorage.getItem("email");
+        console.log(user)
+        fetch("http://localhost:5000/users/details/" + user)
+          .then((response) => response.json())
+          .then((data) => setUsers(data));
+      };
   return (
     <div>
       <Navbar />
       <section>
         <div class="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-11xl">
-          <div class="grid w-full grid-cols-1 gap-12 mx-auto lg:grid-cols-3">
+          <div class="grid items-center w-full grid-cols-1 gap-12 mx-auto lg:grid-cols-2 shadow-xl">
             <div class="p-6">
-              <Portter />
+                {details ? <Portter />:<><Dischelp/></>}
+              
               
             </div>
 
             <div class="p-6">
-              <Dischelp />
-           
-            </div>
-            <div class="p-6">
-              <Userter />
+                {details ? <><Dischelp/></>:<Userter />}
+              
              
             </div>
           </div>
