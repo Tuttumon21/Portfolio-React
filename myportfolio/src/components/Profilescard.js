bdb702e02f544b8812777982fac78f7b6c86a15d
RakeshKR <Rakeshkr212000@gmail.com>
2023-03-11 13:45:04 +0530

    category fix
    category fixed, based on details have data. if the profile available then shows profiles, if particular category have no details then shows no profiles and shows  empty text.

--- myportfolio/src/components/Profilescard.js
+++ myportfolio/src/components/Profilescard.js
@@ -4,15 +4,25 @@ import Emptytext from "./text/Emptytext";
 
 const Profilescard = ({ category }) => {
   const [details, setDetails] = useState(null);
+
   useEffect(() => {
     fetch("http://localhost:5000/portfolio/" + category)
       .then((response) => response.json())
-      .then((data) => setDetails(data));
-  }, []);
+      .then((data) => {
+        // Filter the profiles based on the selected category
+        const filteredProfiles = category === "all" ? data : data.filter(profile => profile.category === category);
+        setDetails(filteredProfiles.length > 0 ? filteredProfiles : null);
+      });
+  }, [category]);
+  
+
+  if (!details) {
+    return <Emptytext />;
+  }
 
   return (
     <div>
-          <Profile details={details} />
+      <Profile details={details} />
     </div>
   );
 };
