29b311ce7a59a809de587c8c6f9584b503476bfe
RakeshKR <Rakeshkr212000@gmail.com>
2023-03-13 01:54:18 +0530

    test
    testing

--- myportfolio/src/components/Test.js
+++ myportfolio/src/components/Test.js
@@ -1,18 +1,24 @@
 // import React from 'react'
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React from 'react';
+import About from './About';
+import Services from './Services';
+import Textintro from './text/Textintro';
+// import { useInView } from 'react-intersection-observer';
 
 const Test = () => {
-  const [details, setDetails] = useState("");
-  useEffect(() => {
-     fetch("http://localhost:5000/portfolio/")
-      .then((response) => response.json())
-      .then((data) => setDetails(data));
-  }, []);
-   
-     
+  // const [details, setDetails] = useState("");
+  // useEffect(() => {
+  //    fetch("http://localhost:5000/portfolio/")
+  //     .then((response) => response.json())
+  //     .then((data) => setDetails(data));
+  // }, []);
+  return (
+    <div>
+    <Textintro/>
+    </div>
+  );
+};
 
-  return(<>hi</>)
-
-}
 
 export default Test
